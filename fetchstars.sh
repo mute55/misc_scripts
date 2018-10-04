bf9626ddb77e0c70bb98a9403d44758713e6fdda
mute <mute55@tfwno.gf>
2018-10-04 22:02:01 +0100

    removed duplicate files

--- fetchstars.sh
+++
@@ -1,1 +0,0 @@
-curl "https://api.github.com/users/mute55/starred?page=1&per_page=100" | grep -e 'git_url*' | cut -d \" -f 4 | xargs -L1 git clone
--- updaterepos.sh
+++
@@ -1,1 +0,0 @@
-find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} fetch \;
